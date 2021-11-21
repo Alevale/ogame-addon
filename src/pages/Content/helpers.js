2e7add62fc49aeebb9a6e8c4294e29ff0870b722
alevale <alexvales111@gmail.com>
2021-11-21 18:39:28 +0100

    impr(helpers): Create a non linear similar function to setInterval

--- src/pages/Content/helpers.js
+++ src/pages/Content/helpers.js
@@ -1,7 +1,42 @@
-export const printLine = (line) => {
-  console.log('===> FROM THE PRINT MODULE:', line);
-};
-
 export const setAttributes = (el, attrs) => {
   Object.entries(attrs).forEach(([key, value]) => el.setAttribute(key, value));
+}
+
+let timeouts = {}
+export const createNonLinearInterval = (callback, maxWaitingTime, minWaitingTime = 0, intervalName) => {
+  let nextExecution
+  do {
+    nextExecution = (Math.random() * maxWaitingTime)
+  } while (nextExecution < minWaitingTime);
+
+  const intervalTimeout = setTimeout(() => {
+    const abort = callback()
+    if (abort) {
+      return
+    }
+    createNonLinearInterval(callback, maxWaitingTime, minWaitingTime, intervalName);
+  }, nextExecution)
+
+  if (intervalName) {
+    if (!timeouts[intervalName]) {
+      timeouts[intervalName] = []
+    }
+    timeouts[intervalName].push(intervalTimeout)
+  }
+
+}
+
+export const stopAllNonLinearIntervals = () => {
+  timeouts.forEach(t => clearTimeout(t))
+  timeouts = []
+}
+
+export const stopNonLinearIntervals = (intervalName) => {
+  if (!intervalName) {
+    throw Error('Missing function parameter IntervalName')
+  }
+  const timeout = timeouts[intervalName]
+  // console.log(timeout)
+  timeout.forEach(t => clearTimeout(t))
+  timeouts[intervalName] = []
 }
