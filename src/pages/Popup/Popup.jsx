4aff8f8a01ea7f8646a31680045e3588bb1ba0b7
alevale <alexvales111@gmail.com>
2021-11-30 19:06:35 +0100

    feat(Popup): Add the option to configure the options you want enabled in the page

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,14 +1,58 @@
 import React, { useState } from 'react';
 import './Popup.css';
 
-const Popup = () => {
+const Popup = ({options, asteroids = {}}) => {
+  const [enabledOptions, setEnabledOptions] = useState(options)
   const [countDownMinutes, setCountDownMinutes] = useState(0)
   const [countDownSeconds, setCountDownSeconds] = useState(0)
+
+  const updateExtensionStorage = (enabledOptionName) => {
+    const newVal = !enabledOptions[enabledOptionName]
+    const newEnabledOptions = {
+      ...enabledOptions,
+      [enabledOptionName]: newVal
+    }
+
+    chrome.storage.local.set({
+      enabledOptions: newEnabledOptions,
+    }, () => {
+
+      chrome.tabs.query({}, (tabs) => {
+        const myTab = tabs.find(t => t.url?.includes('ogamex'));
+        chrome.tabs.sendMessage(myTab.id, 'enabledOptions-updated', (response) => {
+          setEnabledOptions(newEnabledOptions);
+        });
+      });
+    });
+  }
+
+  const cleanUp = () => {
+    chrome.storage.local.set({ asteroids: {} }, () => {
+    });
+  }
+
+  const openLink = (position) => {
+    window.open(`/fleet?x=3&y=${position}&z=17&mission=12`)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        
+
+        { Object.keys(asteroids).map((position) => {
+          return <div onClick={ () => openLink(position) }>{position} expires in {asteroids[position]}</div>
+        }) }
+
+        <button onClick={ cleanUp }>
+          Cleanup asteroids
+        </button>
+
+        { Object.keys(enabledOptions).map((enabledOptionName) => {
+          return <button onClick={ () => updateExtensionStorage(enabledOptionName) } key={enabledOptionName}>
+            {enabledOptionName} is { enabledOptions[enabledOptionName] ? 'on' : 'off' }
+          </button>
+        }) }
+
         Min: <input type='number' value={ countDownMinutes} onChange={ v => setCountDownMinutes(v.target.value) } />
         Sec: <input type='number' value={ countDownSeconds} onChange={ v => setCountDownSeconds(v.target.value) } />
         <button onClick={() => {
--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -4,6 +4,19 @@ import { render } from 'react-dom';
 import Popup from './Popup';
 import './index.css';
 
-render(<Popup />, window.document.querySelector('#app-container'));
+const presetEnabledOptions = {
+};
+
+chrome.storage.local.get(null, ({ enabledOptions, asteroids = {} }) => {
+  if (typeof enabledOptions === 'undefined' || Object.keys(enabledOptions).length !== Object.keys(presetEnabledOptions).length) {
+    chrome.storage.local.set({ enabledOptions: presetEnabledOptions }, () => {
+      render(<Popup options={ enabledOptions } asteroids={asteroids} />, window.document.querySelector('#app-container'));
+    });
+    return;
+  }
+
+  render(<Popup options={ enabledOptions } asteroids={asteroids} />, window.document.querySelector('#app-container'));
+});
+
 
 if (module.hot) module.hot.accept();
